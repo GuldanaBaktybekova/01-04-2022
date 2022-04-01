c9670547071a9e6fc53e498eb869b9354ddd7e8f
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-04-01 14:12:00 +0600

    Added the first example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
 function App() {
+  const example1 = ["apple", "red", "big"];
   return (
     <div className="App">
-     
+     This {example1[0]} is {example1[1]} and {example1[2]}
     </div>
   );
 }
